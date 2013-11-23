586e323f7f6c7769a234b10cefeccd61b2079ff9
McSherry <liamj.mcsherry@googlemail.com>
2013-11-23 23:18:21 +0000

    rsa shit might be slightly less fucked up

--- Server/Crypto/Rsa.cs
+++ Server/Crypto/Rsa.cs
@@ -8,85 +8,50 @@ using System.Security.Cryptography;
 namespace AppsAgainstHumanity.Server.Crypto
 {
     using BytePair = KeyValuePair<byte[], byte[]>;
-    using QuadPair = KeyValuePair<KeyValuePair<byte[], byte[]>, KeyValuePair<byte[], byte[]>>;
+    using StringPair = KeyValuePair<string, string>;
 
     public class Rsa
     {
-        public static QuadPair GenerateKeyPair()
+        public static StringPair GenerateKeyPair()
         {
-            BytePair rsaPriv,rsaPubl;
+            StringPair rsaKeys;
             using (var rsa = new RSACryptoServiceProvider(2048) { PersistKeyInCsp = false })
             {
-                // Public key is { exponent, modulus }
-                rsaPubl = new BytePair(rsa.ExportParameters(false).Exponent, rsa.ExportParameters(false).Modulus);
-                // Public key is { private exponent, modulus }
-                rsaPriv = new BytePair(rsa.ExportParameters(true).D, rsa.ExportParameters(true).Modulus);
+                // Public key
+                rsaKeys = new StringPair(
+                    rsa.ToXmlString(false), // Public Key
+                    rsa.ToXmlString(true) // Private Key
+                );
             }
 
-            return new KeyValuePair<BytePair, BytePair>(rsaPriv, rsaPubl);
+            return rsaKeys;
         }
 
-        public byte[] PublicKeyEncrypt(byte[] plaintext, BytePair publicKey)
+        public byte[] PublicKeyEncrypt(byte[] plaintext, string publicKey)
         {
             RSACryptoServiceProvider rsa = new RSACryptoServiceProvider() { PersistKeyInCsp = false };
 
-            rsa.ImportParameters(
-                new RSAParameters()
-                {
-                    Exponent = publicKey.Key,
-                    Modulus = publicKey.Value
-                }
-            );
+            rsa.FromXmlString(publicKey);
 
             return rsa.Encrypt(plaintext, false);
         }
-        public byte[] PrivateKeyDecrypt(byte[] ciphertext, BytePair privateKey)
+        public byte[] PrivateKeyDecrypt(byte[] ciphertext, string privateKey)
         {
             RSACryptoServiceProvider rsa = new RSACryptoServiceProvider() { PersistKeyInCsp = false };
 
-            rsa.ImportParameters(
-                new RSAParameters()
-                {
-                    D = privateKey.Key,
-                    Modulus = privateKey.Value
-                }
-            );
+            rsa.FromXmlString(privateKey);
 
             return rsa.Decrypt(ciphertext, false);
         }
 
-        // NOTE: UNTESTED
-        // THIS MAY NOT, AND LIKELY WILL NOT WORK
-        // USED AS A WORKAROUND FOR MICROSOFT'S SIGN/VERIFY METHODS
-        // ATTEMPTS ENCRYPTION WITH PUBLIC KEY (RATHER THAN PRIVATE AS ABOVE)
-        // ATTEMPTS DECRYPTION WITH PRIVATE KEY (RATHER THAN PUBLIC AS ABOVE)
-        public byte[] Sign(byte[] plaintext, BytePair privateKey)
+        public byte[] Sign(byte[] plaintext, string privateKey)
         {
-            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider() { PersistKeyInCsp = false };
-
-            rsa.ImportParameters(
-                new RSAParameters()
-                {
-                    Exponent = privateKey.Key,
-                    Modulus = privateKey.Value
-                }
-            );
-
-            return rsa.Encrypt(plaintext, false);
+            return this.PrivateKeyDecrypt(plaintext, privateKey);
         }
-        public bool Verify(byte[] ciphertext, BytePair publicKey, byte[] hashValue)
+        public bool Verify(byte[] ciphertext, string publicKey, byte[] hashValue)
         {
-            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider() { PersistKeyInCsp = false };
 
-            rsa.ImportParameters(
-                new RSAParameters()
-                {
-                    D = publicKey.Key,
-                    Modulus = publicKey.Value
-                }
-            );
-
-            return rsa.Decrypt(ciphertext, false).SequenceEqual(hashValue);
+            return this.PublicKeyEncrypt(ciphertext, publicKey).SequenceEqual(hashValue);
         }
     }
 }
