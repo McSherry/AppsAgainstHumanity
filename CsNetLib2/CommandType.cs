8322205b65fe6a3f7d633dc6e0d6a1299c06ed30
McSherry <liamj.mcsherry@googlemail.com>
2013-12-08 10:38:38 +0000

    added PING to CommandType

--- CsNetLib2/CommandType.cs
+++ CsNetLib2/CommandType.cs
@@ -36,6 +36,7 @@ namespace CsNetLib2
         CRTO,
         CZTO,
         UNRG,
+        PING,
         // For use when an invalid command is sent to AAHProtocolWrapper.
         // Recommended behaviour for handlers registered to this on a server is to reply with UNRG.
         // Recommended behaviour for clients is not to bind to this.
